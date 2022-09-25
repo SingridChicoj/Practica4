29370bd3229a02cb1c599d62f2188e59e0fa1b77
Andrés Emilio Peñate <3428938822207@ingenieria.usac.edu.gt>
2022-09-24 18:09:50 -0600

    6:07 PM
    1

--- Informe4/Informe4/src/app/app.module.ts
+++ Informe4/Informe4/src/app/app.module.ts
@@ -15,14 +15,37 @@ import { AppRoutingModule } from "./app-routing.module"
 
 =======
 import { HomeComponent } from './home/home.component';
+<<<<<<< Updated upstream
 >>>>>>> a8d81836a3e65bc3ad7a687db25d63264cc2c884
+=======
+<<<<<<< HEAD
+import { RegistroComponent } from './registro/registro.component';
+import { PublicacionesComponent } from './publicaciones/publicaciones.component';
+import { PublicarComponent } from './publicar/publicar.component';
+import { ComentarComponent } from './comentar/comentar.component';
+import { ComentariosComponent } from './comentarios/comentarios.component';
+import { InfoUsuarioComponent } from './info-usuario/info-usuario.component';
+import { EditarUsuarioComponent } from './editar-usuario/editar-usuario.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+=======
+>>>>>>> a8d81836a3e65bc3ad7a687db25d63264cc2c884
+>>>>>>> 0ea86111606192b93abde18daccdd554d6c4fd7d
+>>>>>>> Stashed changes
 
 @NgModule({
   //declaracion de componentes
   declarations: [
     AppComponent,
     LoginComponent,
-    HomeComponent
+    HomeComponent,
+    RegistroComponent,
+    PublicacionesComponent,
+    PublicarComponent,
+    ComentarComponent,
+    ComentariosComponent,
+    InfoUsuarioComponent,
+    EditarUsuarioComponent,
+    DashboardComponent
   ],
 
   //importacion de modulos
---
+++ Informe4/Informe4/src/app/comentar/comentar.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ComentarComponent } from './comentar.component';
+
+describe('ComentarComponent', () => {
+  let component: ComentarComponent;
+  let fixture: ComponentFixture<ComentarComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ComentarComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ComentarComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
