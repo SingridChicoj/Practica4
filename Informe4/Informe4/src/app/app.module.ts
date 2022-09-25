7c75af684fb8b6f2d8e608303adda553bb186334
Andrés Emilio Peñate <3428938822207@ingenieria.usac.edu.gt>
2022-09-25 00:21:38 -0600

    12:21

--- Informe4/Informe4/src/app/app.module.ts
+++ Informe4/Informe4/src/app/app.module.ts
@@ -6,19 +6,16 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 
 import {HttpClientModule} from '@angular/common/http';
-<<<<<<< HEAD
+
 import { FormsModule } from "@angular/forms"
 
 import { AppRoutingModule } from "./app-routing.module"
 
 //para rutas
 
-=======
+
 import { HomeComponent } from './home/home.component';
-<<<<<<< Updated upstream
->>>>>>> a8d81836a3e65bc3ad7a687db25d63264cc2c884
-=======
-<<<<<<< HEAD
+
 import { RegistroComponent } from './registro/registro.component';
 import { PublicacionesComponent } from './publicaciones/publicaciones.component';
 import { PublicarComponent } from './publicar/publicar.component';
@@ -27,10 +24,7 @@ import { ComentariosComponent } from './comentarios/comentarios.component';
 import { InfoUsuarioComponent } from './info-usuario/info-usuario.component';
 import { EditarUsuarioComponent } from './editar-usuario/editar-usuario.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-=======
->>>>>>> a8d81836a3e65bc3ad7a687db25d63264cc2c884
->>>>>>> 0ea86111606192b93abde18daccdd554d6c4fd7d
->>>>>>> Stashed changes
+
 
 @NgModule({
   //declaracion de componentes
