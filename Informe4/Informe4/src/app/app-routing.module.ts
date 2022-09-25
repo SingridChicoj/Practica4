b00e30c7e7aca9717fec335149dc600cf6c00b08
Lu1s16 <3045905330115@ingenieria.usac.edu.gt>
2022-09-25 17:34:51 -0600

    prueba

--- Informe4/Informe4/src/app/app-routing.module.ts
+++ Informe4/Informe4/src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from "@angular/core"
 import { Routes, RouterModule } from "@angular/router"
 import { LoginComponent } from "./login/login.component"
+import { HomeComponent } from "./home/home.component"
+
 
 const routes: Routes = [
     //rutas de la pagina
 
     { path:"", redirectTo:"/", pathMatch: "full" },
-    { path:"login", component: LoginComponent} //ruta con componente
+    { path:"login", component: LoginComponent}, //ruta con componente
+    { path:"/", component: HomeComponent}
 
 ]
 
--- Informe4/Informe4/src/app/home/home.component.ts
+++ Informe4/Informe4/src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { HomeService } from "../home.service"
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private ServicioHome:HomeService) { }
 
   ngOnInit(): void {
+
+    this.home()
+
+
+
+
   }
+
+  home(){
+    this.ServicioHome.home().subscribe(datos=>{
+      console.log(datos)
+    },
+    
+    datos => console.log("Error")
+
+    )
+
+    
+    
+
+  }
+
+
 
 }
