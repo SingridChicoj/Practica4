af11ec690a7aeb1d225516a3687b14e390b35cdd
Andrés Emilio Peñate <3428938822207@ingenieria.usac.edu.gt>
2022-09-26 00:01:00 -0600

    12:00 AM
    Correccion error en login

--- Informe4/Informe4/src/app/Todo/login/login.component.ts
+++ Informe4/Informe4/src/app/Todo/login/login.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup,FormControl, Validators } from '@angular/forms';
 import { LoginI } from 'src/app/Modelos/Login';
 import {PrincipalService}from '../../Services/principal.service';
 import { Router, RouterEvent } from '@angular/router';
 import { TestI } from 'src/app/Modelos/Test';
+
 
 
 //importo login.service.ts donde esta la funcion
@@ -36,3 +36,4 @@ export class LoginComponent implements OnInit {
   }
 
 }
+
